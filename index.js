9d416b2f4485647021345272e38246e32da88a75
safiullah <safiahmadzai26@gmail.com>
2024-01-22 12:20:45 +0430

    initial commit

---
+++ index.js
@@ -0,0 +1,85 @@
+// // // // // document.getElementById("count-el").innerHTML=5;
+// // // // // document.querySelector("#count-el").textContent=7;
+// // // // // document.getElementById("count-el").innerHTML=6;
+// // // // // let myage=22;
+// // // // // console.log(myage);
+// // // // // let number=0;
+// // // // // console.log(number);
+ 
+// // // // let myage=1;
+// // // // let dogage=7;
+// // // // let mydogage=myage*dogage;
+// // // // console.log(mydogage);
+
+// // // let bunospoint=50;
+// // // bunospoint=bunospoint*2;
+// // // console.log(bunospoint);
+// // // bunospoint=bunospoint-25;
+// // // console.log(bunospoint);
+// // // bunospoint=bunospoint/3;
+// // // console.log(bunospoint);
+
+
+
+// // let count=0;
+// // document.getElementById("increment-btn").addEventListener("click",function(){
+// //     count=count+1;
+// //     document.getElementById("count-el").textContent=count;
+// // })
+
+// // function increment(){
+// //     console.log("the button was clicked");
+// // }
+// function countdown(){
+//     console.log(5)
+//     console.log(4)
+//     console.log(3)
+//     console.log(2)
+//     console.log(1)
+// }
+// countdown();
+// countdown();
+
+
+// function logger(){
+//     console.log(42);
+// }
+// logger();
+// let lap1=20;
+// let lap2=20;
+// let lap3=20;
+
+// function sumoflap(){
+// let total=lap1+lap2+lap3;
+// console.log(total)
+// }
+
+
+// sumoflap();
+
+
+// let latcom=0;
+// function lapcom(){
+//     latcom=latcom+1;
+// }
+// lapcom();
+// lapcom();
+// lapcom();
+// console.log(latcom);
+let firsr_string="hello messsage";
+let username="per";
+console.log(firsr_string);
+console.log(username);
+
+
+let count_el=document.getElementById("count-el");
+
+let count=0;
+function increment(){
+    count=count+1;
+   count_el.innerText=count;
+
+}
+function save(){
+console.log(count);
+}
